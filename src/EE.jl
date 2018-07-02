42ea2cb450472380a310d007e316aca816144be3
Christian Kral <dr.christian.kral@gmail.com>
2018-07-02 23:15:11 +0200

    Add documentation of function phasor

--- src/EE.jl
+++ src/EE.jl
@@ -6,6 +6,73 @@ module EE
 
     export phasor
 
+    """
+    # Function call
+
+    `phasor(c;origin=0.0+0.0im,ref=1,par=0,
+        rlabel=0.5,tlabel=0.25,label="",ha="center",va="center",
+        relrot=false,relangle=0,
+        color="black",width=0.2,headlength=10,headwidth=5)`
+
+    # Description
+
+    This function draws a phasor from a starting point `origin` and end point
+    `origin`+`c`. The phasor consists of a shaft and an arrow head.
+
+    # Variables
+
+    `c` Complex phasor, drawn relative relative to `B`
+
+    `origin` Complex number representing the origin of the phasor
+
+    `ref` Reference length of scaling; this is required as in a pahasor diagram
+    voltages and currents may be included; in order to account for the different
+    voltage and current scales, one (constant)  `ref` is used for voltage
+    phasors and another (constant) `ref` is used for current phasors
+
+    `par` In order to be able to plot parallel phasors, par is used to specify
+    the tangential shift (offset) of a phasor, with respect to `ref`; so
+    typlically `ref` will be selected to be around 0.05 to 0.1;
+    default value = 0 (no shift of phasor)
+
+    `rlabel` radial location of label (in direction of the phasor):
+    `rlabel = 0` represents the shaft of the phasor and `rlabel = 1` represents
+    the arrow hear of the phasor; default value = 0.5, i.e., the radial center
+    of the phasor
+
+    `tlabel` tangential location of label: `tlabel = 0` means that the label is
+    plotted onto the phasor; `tlabel = -0.25` plots the label on top of the
+    phasor applying a displacement of 25% with respect to `ref`;
+    `tlabel = 0.2` plots the label below the
+    phasor applying a displacement of 20% with respect to `ref`; default value
+    = 0.25
+
+    `ha` horizontal alignment of label; actually represents the tangential
+    alignment of the label; default value = "center"
+    `linewidth` Line width of arrow shaft
+
+    `va` vertical alignment of label; actually represents the radial
+    alignment of the label; default value = "center"
+
+    `relrot` relative rotation of label; if `relrot == false` (default value)
+    then the label is not rotated relative to the orientation of the phasor;
+    otherwise the label is rotated relative to the phasor by the angle
+    `relangle` (indicated in degrees)
+
+    `relangle` relative angle of label with respect to phasor orientient; this
+    angle is only appplied, it `relrot == true`; this angle the indicates the
+    relative orientation of the label with respect to the orientation of the
+    phasor; default value = 0
+
+    `color` color of the phasor; i.e., shaft and arrow head color; default
+    value = "black"
+
+    `width` line width of the shaft line; default value = 0.2
+
+    `headlength` lenght of arrow head; default value = 10
+
+    `headwidth` width of arrow head; default value = 5
+    """
     function phasor(c;origin=0.0+0.0im,ref=1,par=0,
         rlabel=0.5,tlabel=0.25,label="",ha="center",va="center",
         relrot=false,relangle=0,
