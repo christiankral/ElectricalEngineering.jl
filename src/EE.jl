ef775c159d71693d2dc643c45f63b4a00e9ea69d
Christian Kral <dr.christian.kral@gmail.com>
2018-07-03 10:56:51 +0200

    Test implementation of vectorization

--- src/EE.jl
+++ src/EE.jl
@@ -86,28 +86,28 @@ module EE
         color="black",width=0.2,headlength=10,headwidth=5)
 
         # Starting point (origin) of phase
-        xorigin=real(origin)/ref
-        yorigin=imag(origin)/ref
+        xorigin=real(origin)./ref
+        yorigin=imag(origin)./ref
         # End point of phasor
-        xend=real(origin+c)/ref
-        yend=imag(origin+c)/ref
+        xend=real(origin+c)./ref
+        yend=imag(origin+c)./ref
         # Real part of phasor
-        drx=real(c)/ref
+        drx=real(c)./ref
         # Imag part of phasor
-        dry=imag(c)/ref
+        dry=imag(c)./ref
         # Length of phasor
-        dr=sqrt(drx^2+dry^2)
+        dr=sqrt.(drx.^2+.dry^2)
         # Angle of phasor in degrees
-        absangle=atan2(dry,drx)*180/pi
+        absangle=atan2.(dry,drx)*180/pi
         # Orientation tangential to phasor (lagging by 90°)
         # Real part of tangential component with repsect to length
-        dtx=+dry/dr
+        dtx=+dry./dr
         # Imag part of tangential component with repsect to length
-        dty=-drx/dr
+        dty=-drx./dr
         # Real part of parallel shift of phasor
-        dpx=par*dtx
+        dpx=par.*dtx
         # Imag part of parallel shift of phasor
-        dpy=par*dty
+        dpy=par.*dty
 
         # Draw arrow
         annotate("",xy=(xend+dpx,yend+dpy),xytext=(xorigin+dpx,yorigin+dpy),
@@ -116,15 +116,17 @@ module EE
                             "headlength"=>headlength,"headwidth"=>headwidth),
                             annotation_clip=false)
         # Plot label
-        if relrot==false
-            # Without relative roation of label
-            text(xorigin+drx*rlabel+dtx*tlabel+dpx,yorigin+dry*rlabel+dty*tlabel+dpy,
-              label,ha=ha,va=va)
-        else
-            # Applying relative rotation of label
-            text(xorigin+drx*rlabel+dtx*tlabel+dpx,yorigin+dry*rlabel+dty*tlabel+dpy,
-              label,ha=ha,va=va,rotation=absangle+relangle)
-        end
+        # if relrot==false
+        #     # Without relative roation of label
+        #     text(xorigin+drx*rlabel+dtx*tlabel+dpx,
+        #         yorigin+dry*rlabel+dty*tlabel+dpy,
+        #         label,ha=ha,va=va)
+        # else
+        #     # Applying relative rotation of label
+        #     text(xorigin+drx*rlabel+dtx*tlabel+dpx,
+        #         yorigin+dry*rlabel+dty*tlabel+dpy,
+        #         label,ha=ha,va=va,rotation=absangle+relangle)
+        # end
     end
 
 end
