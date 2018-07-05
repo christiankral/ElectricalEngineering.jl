49148d474962a78bef77ebc031fef0406b8d4c7b
Christian Kral <dr.christian.kral@gmail.com>
2018-07-05 23:35:13 +0200

    Add colors schemes associated to quantities

---
+++ src/Tab20bc.jl
@@ -0,0 +1,68 @@
+module PlotColorTab20
+
+  # Assign colors to quanities based on 
+  # color schemes tab20c and tab20b taken from
+  # https://matplotlib.org/users/plotting/colormaps/grayscale_01_04.pdf
+
+  const colorVoltage1 = colorBlue1
+  const colorVoltage2 = colorBlue2
+  const colorVoltage3 = colorBlue3
+  const colorVoltage4 = colorBlue4
+
+  const colorCurrent1 = colorRed1
+  const colorCurrent2 = colorRed2
+  const colorCurrent3 = colorRed3
+  const colorCurrent4 = colorRed4
+
+  const colorPower1 = colorPurple1
+  const colorPower2 = colorPurple2
+  const colorPower3 = colorPurple3
+  const colorPower4 = colorPurple4
+
+  const colorTorque1 = colorBrown1
+  const colorTorque2 = colorBrown2
+  const colorTorque3 = colorBrown3
+  const colorTorque4 = colorBrown4
+
+  const colorSpeed1 = colorMagenta1
+  const colorSpeed2 = colorMagenta2
+  const colorSpeed3 = colorMagenta3
+  const colorSpeed4 = colorMagenta4
+
+  const colorSignal1 = colorDarkPurple1
+  const colorSignal2 = colorDarkPurple2
+  const colorSignal3 = colorDarkPurple3
+  const colorSignal4 = colorDarkPurple4
+
+  const colorBoolean1 = colorMagenta1
+  const colorBoolean2 = colorMagenta2
+  const colorBoolean3 = colorMagenta3
+  const colorBoolean4 = colorMagenta4
+
+  const colorMagneticVoltage1 = colorOrange1
+  const colorMagneticVoltage2 = colorOrange2
+  const colorMagneticVoltage3 = colorOrange3
+  const colorMagneticVoltage4 = colorOrange4
+
+  const colorFlux1 = colorGreen1
+  const colorFlux2 = colorGreen2
+  const colorFlux3 = colorGreen3
+  const colorFlux4 = colorGreen4
+
+  const colorTemperature1 = colorRed1
+  const colorTemperature2 = colorRed2
+  const colorTemperature3 = colorRed3
+  const colorTemperature4 = colorRed4
+
+
+  export colorVoltage1,colorVoltage2,colorVoltage3,colorVoltage4,
+         colorCurrent1,colorCurrent2,colorCurrent3,colorCurrent4,
+         colorPower1,colorPower2,colorPower3,colorPower4,
+         colorTorque1,colorTorque2,colorTorque3,colorTorque4,
+         colorSpeed1,colorSpeed2,colorSpeed3,colorSpeed4,
+         colorSignal1,colorSignal2,colorSignal3,colorSignal4,
+         colorBoolean1,colorBoolean2,colorBoolean3,colorBoolean4,
+         colorMagneticVoltage1,colorMagneticVoltage2,colorMagneticVoltage3,colorMagneticVoltage4,
+         colorFlux1,colorFlux2,colorFlux3,colorFlux4,
+         colorTemperature1,colorTemperature2,colorTemperature3,colorTemperature4
+end
