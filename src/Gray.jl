cf46bb0a9a59d9cfc8e159d781f3a6adc5b9165e
Christian Kral <dr.christian.kral@gmail.com>
2018-07-05 23:35:40 +0200

    Add gray color scheme associated to quantities

---
+++ src/Gray.jl
@@ -0,0 +1,67 @@
+module PlotColorGray
+
+  # Assign colors to quanities  based on 
+  # gray scales colorBlack1..4
+
+  const colorVoltage1 = colorBlack1
+  const colorVoltage2 = colorBlack2
+  const colorVoltage3 = colorBlack3
+  const colorVoltage4 = colorBlack4
+
+  const colorCurrent1 = colorBlack1
+  const colorCurrent2 = colorBlack2
+  const colorCurrent3 = colorBlack3
+  const colorCurrent4 = colorBlack4
+
+  const colorPower1 = colorBlack1
+  const colorPower2 = colorBlack2
+  const colorPower3 = colorBlack3
+  const colorPower4 = colorBlack4
+
+  const colorTorque1 = colorBlack1
+  const colorTorque2 = colorBlack2
+  const colorTorque3 = colorBlack3
+  const colorTorque4 = colorBlack4
+
+  const colorSpeed1 = colorBlack1
+  const colorSpeed2 = colorBlack2
+  const colorSpeed3 = colorBlack3
+  const colorSpeed4 = colorBlack4
+
+  const colorSignal1 = colorBlack1
+  const colorSignal2 = colorBlack2
+  const colorSignal3 = colorBlack3
+  const colorSignal4 = colorBlack4
+
+  const colorBoolean1 = colorBlack1
+  const colorBoolean2 = colorBlack2
+  const colorBoolean3 = colorBlack3
+  const colorBoolean4 = colorBlack4
+
+  const colorMagneticVoltage1 = colorBlack1
+  const colorMagneticVoltage2 = colorBlack2
+  const colorMagneticVoltage3 = colorBlack3
+  const colorMagneticVoltage4 = colorBlack4
+
+  const colorFlux1 = colorBlack1
+  const colorFlux2 = colorBlack2
+  const colorFlux3 = colorBlack3
+  const colorFlux4 = colorBlack4
+
+  const colorTemperature1 = colorBlack1
+  const colorTemperature2 = colorBlack2
+  const colorTemperature3 = colorBlack3
+  const colorTemperature4 = colorBlack4
+
+
+  export colorVoltage1,colorVoltage2,colorVoltage3,colorVoltage4,
+         colorCurrent1,colorCurrent2,colorCurrent3,colorCurrent4,
+         colorPower1,colorPower2,colorPower3,colorPower4,
+         colorTorque1,colorTorque2,colorTorque3,colorTorque4,
+         colorSpeed1,colorSpeed2,colorSpeed3,colorSpeed4,
+         colorSignal1,colorSignal2,colorSignal3,colorSignal4,
+         colorBoolean1,colorBoolean2,colorBoolean3,colorBoolean4,
+         colorMagneticVoltage1,colorMagneticVoltage2,colorMagneticVoltage3,colorMagneticVoltage4,
+         colorFlux1,colorFlux2,colorFlux3,colorFlux4,
+         colorTemperature1,colorTemperature2,colorTemperature3,colorTemperature4
+end
