865faac42bda1ef0e545c58e686347c62a8f6826
Christian Kral <dr.christian.kral@gmail.com>
2018-07-06 06:27:13 +0200

    Fix missing colorBlack1-5

--- src/plot.jl
+++ src/plot.jl
@@ -1,33 +1,34 @@
-export lineStyle1,lineStyle2,lineStyle3,lineStyle4,
-       lineWidth1,lineWidth2,lineWidth3,lineWidth4,
-       marker1,marker2,marker3,marker4,
-       markerSize1,markerSize2,markerSize3,markerSize4,
-       legendFontSize
+export colorBlack1,colorBlack2,colorBlack3,colorBlack4,colorBlack5,
+    lineStyle1,lineStyle2,lineStyle3,lineStyle4,
+    lineWidth1,lineWidth2,lineWidth3,lineWidth4,
+    marker1,marker2,marker3,marker4,
+    markerSize1,markerSize2,markerSize3,markerSize4,
+    legendFontSize
 
-  const colorBlack1="0"
-  const colorBlack2="0.25"
-  const colorBlack3="0.5"
-  const colorBlack4="0.6"
-  const colorBlack5="0.85"
+const colorBlack1="0"
+const colorBlack2="0.25"
+const colorBlack3="0.5"
+const colorBlack4="0.6"
+const colorBlack5="0.85"
 
-  const lineStyle1="-"
-  const lineStyle2=linestyle=(0,(4,1))
-  const lineStyle3=linestyle=(0,(2,0.81))
-  const lineStyle4=linestyle=(0,(8,1))
+const lineStyle1="-"
+const lineStyle2=linestyle=(0,(4,1))
+const lineStyle3=linestyle=(0,(2,0.81))
+const lineStyle4=linestyle=(0,(8,1))
 
-  const lineWidth1=1
-  const lineWidth2=1
-  const lineWidth3=1.5
-  const lineWidth4=1
+const lineWidth1=1
+const lineWidth2=1
+const lineWidth3=1.5
+const lineWidth4=1
 
-  const marker1="D"
-  const marker2="o"
-  const marker3="s"
-  const marker4="*"
+const marker1="D"
+const marker2="o"
+const marker3="s"
+const marker4="*"
 
-  const markerSize1=4
-  const markerSize2=4
-  const markerSize3=4
-  const markerSize4=5
+const markerSize1=4
+const markerSize2=4
+const markerSize3=4
+const markerSize4=5
 
-  const legendFontSize=10
+const legendFontSize=10
