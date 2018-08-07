fe702e04e53412d94c03904d62741b8a89dd6ec7
Christian Kral <dr.christian.kral@gmail.com>
2018-08-07 13:27:40 +0200

    Update example

--- src/circuit.jl
+++ src/circuit.jl
@@ -14,7 +14,7 @@ calculation can be performed using units from the module Unitiful.
 
 **Important note:** Use the ∥ operator in parentheses, since its precedence
 is equal to the (low) precedence of relation operators or use the function call
-with arguments to avoid miscalculations 
+with arguments to avoid miscalculations
 
 # Variables
 
@@ -28,9 +28,11 @@ julia> ∥(4,6)
 2.4
 julia> ∥(4Ω,6Ω)
 2.4 Ω
-julia> 2Ω+(4Ω∥6Ω) # Parntheses are required since != 2Ω+4Ω∥6Ω -> 3Ω
-4.4 Ω0
-julia> 2.4Ω∥(4Ω∥6Ω)
+julia> 2Ω+(4Ω∥6Ω) # Parentheses are ALWAYS required since
+4.4 Ω
+julia> 2Ω+4Ω∥6Ω # Is equal to (2Ω+4Ω)∥6Ω which is usually NOT intended
+3.0 Ω
+julia> (2.4Ω∥(4Ω∥6Ω))
 1.2 Ω
 julia> ∥(4Ω,6Ω,2.4Ω)
 1.2 Ω
