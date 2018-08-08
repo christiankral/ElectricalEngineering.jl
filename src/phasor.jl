dc34f080223083264355a447878cfd2fe336c6ae
Christian Kral <dr.christian.kral@gmail.com>
2018-08-08 09:51:16 +0200

    Fixed llinestyle issue of phasor
    The phasor is now drawn as two parts, shaft and head, using different annotation parameters

--- src/phasor.jl
+++ src/phasor.jl
@@ -42,7 +42,7 @@ doc"""
 `phasor(c;origin=0.0+0.0im,ref=1,par=0,
     rlabel=0.5,tlabel=0.1,label="",ha="center",va="center",
     relrot=false,relangle=0,
-    color="black",width=0.2,headlength=10,headwidth=5)`
+    color="black",linesstyle="-",width=0.2,headlength=10,headwidth=5)`
 
 # Description
 
@@ -73,43 +73,45 @@ the per unit tangential shift (offset) of a phasor, with respect to `ref`;
 so typically `ref` will be selected to be around 0.05 to 0.1;
 default value = 0 (no shift of phasor)
 
-`rlabel` radial per unit location of label (in direction of the phasor):
+`rlabel` Radial per unit location of label (in direction of the phasor):
 `rlabel = 0` represents the shaft of the phasor and `rlabel = 1` represents
 the arrow hear of the phasor; default value = 0.5, i.e., the radial center
 of the phasor
 
-`tlabel` tangential per unit location of label: `tlabel = 0` means that the
+`tlabel` Tangential per unit location of label: `tlabel = 0` means that the
 label is plotted onto the phasor; `tlabel = -0.25` plots the label on top of
 the phasor applying a displacement of 25% with respect to `ref`;
-`tlabel = 0.2` plots the label below the
+`tlabel` Plots the label below the
 phasor applying a displacement of 20% with respect to `ref`; default value
 = 0.25
 
-`ha` horizontal alignment of label; actually represents the tangential
+`ha` Horizontal alignment of label; actually represents the tangential
 alignment of the label; default value = "center"
 `linewidth` Line width of arrow shaft
 
-`va` vertical alignment of label; actually represents the radial
+`va` Vertical alignment of label; actually represents the radial
 alignment of the label; default value = "center"
 
-`relrot` relative rotation of label; if `relrot == false` (default value)
+`relrot` Relative rotation of label; if `relrot == false` (default value)
 then the label is not rotated relative to the orientation of the phasor;
 otherwise the label is rotated relative to the phasor by the angle
 `relangle` (indicated in degrees)
 
-`relangle` relative angle of label in degree with respect to phasor
+`relangle` Relative angle of label in degree with respect to phasor
 orientation; this angle is only applied, it `relrot == true`; this angle the
 indicates the relative orientation of the label with respect to the
 orientation of the phasor; default value = 0
 
-`color` color of the phasor; i.e., shaft and arrow head color; default
+`color` Color of the phasor; i.e., shaft and arrow head color; default
 value = "black"
 
-`width` line width of the shaft line; default value = 0.2
+`linestyle` Line style of the phasor; default value = "-"
 
-`headlength` length of arrow head; default value = 10
+`width` Line width of the shaft line; default value = 0.2
 
-`headwidth` width of arrow head; default value = 5
+`headlength` Length of arrow head; default value = 10
+
+`headwidth` Width of arrow head; default value = 5
 
 # Example
 
@@ -117,7 +119,6 @@ Copy and paste code:
 
 ```julia
 using Unitful,Unitful.DefaultSymbols,PyPlot,EE
-
 figure(figsize=(3.3,2.5))
 rc("text",usetex=true);
 rc("font", family="serif")
@@ -144,7 +145,7 @@ function phasor(c;origin=(0.0+0.0im).*c./ustrip(c),
     label="",
     ha="center",va="center",
     relrot=false,relangle=0.0,
-    color="black",width=0.2,
+    color="black",linestyle="-",width=0.2,
     headlength=10.0,headwidth=5.0)
 
     # Check if units of c, origin and ref are compatible
@@ -180,11 +181,26 @@ function phasor(c;origin=(0.0+0.0im).*c./ustrip(c),
     dpx=par*dtx
     # Imag part of parallel shift of phasor
     dpy=par*dty
-    # Draw arrow
+    # Origin of head
+    xoriginHead = xorigin + drx*0.99
+    yoriginHead = yorigin + dry*0.99
+    # Draw arrow shaft and head
+    # https://matplotlib.org/api/_as_gen/matplotlib.pyplot.annotate.html?highlight=annotate#matplotlib.pyplot.annotate
+    # Draw shaft separately: otherwise, the arrow contour will be drawn as in
+    # https://matplotlib.org/users/annotations.html#basic-annotation
+    # so that the back and forth paths overlap and the line style does not
+    # appear correctly
     annotate("",xy=(xend+dpx,yend+dpy),
         xytext=(xorigin+dpx,yorigin+dpy),xycoords="data",
+        arrowprops=Dict("arrowstyle"=>"-","linestyle"=>linestyle,
+           "facecolor"=>color),
+        annotation_clip=false)
+    # Draw arrow head without line style; this is a workaround explained in
+    # https://stackoverflow.com/questions/47180328/pyplot-dotted-line-with-fancyarrowpatch/47205418#47205418
+    annotate("",xy=(xend+dpx,yend+dpy),
+        xytext=(xoriginHead+dpx,yoriginHead+dpy),xycoords="data",
         arrowprops=Dict("edgecolor"=>color,"facecolor"=>color,
-            "width"=>width,"headlength"=>headlength,
+            "width"=>width,"linestyle"=>"-","headlength"=>headlength,
             "headwidth"=>headwidth),
         annotation_clip=false)
 
