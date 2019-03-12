e11c2b0435e3f87c9dc7102e8e2d86afe1d67497
Christian Kral <dr.christian.kral@gmail.com>
2019-03-12 15:59:55 +0100

    Add tests

---
+++ test/runtests.jl
@@ -0,0 +1,13 @@
+# test/runtests.jl
+using EE, Unitful, PyPlot, Test
+
+function tests()
+  @testset "Subset of tests" begin
+    @test ∥(2,3) ≈ 1.2 atol=1E-6
+    @test abs(pol(1,45°)) ≈ 1 atol=1E-6
+    @test angle(pol(1,45°)) ≈ π/4 atol=1E-6
+    @test angle(∠(45°)) ≈ π/4 atol=1E-6
+  end
+end
+
+tests()
