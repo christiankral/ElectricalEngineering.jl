c795bfa5ae745ff6b307d890164adb2f913c8426
Christian Kral <dr.christian.kral@gmail.com>
2024-09-13 18:12:21 +0200

    Add new functions phasorsine_hline and phasorcosine_hline

--- src/phasor.jl
+++ src/phasor.jl
@@ -811,6 +811,192 @@ end
 # Function call
 
 ```
+phasorsine_hline(phasorcosine_hline(mag = 1, phi = 0; color="black",
+linewidth = 1, linestyle = ":", colorDash="gray", shift = true, marker="")
+```
+
+# Description
+
+This function may be applied after using `phasorsine`. It draws horizontal
+line between a phasor on the left subplot and the sine wave on the right
+subplot.
+# Variables
+
+`mag` Magnitude of displayed phasor; shall be between 0 and 1;
+default value = 1
+
+`phi` Phase angle of the phasor; default value = 0
+
+`color` Color of the phasor; i.e., shaft and arrow head color; default
+value = "black"
+
+`linewidth` Line width of the horizontal line; default value = 1
+
+`linestyle` Line style of the horizontal line; default value = ":"
+
+`colorDash` Color of the dashed circle (left subplot) and the horizontal dashed
+lines between the left and right subplot; default value = "gray"
+
+`shift` If `true`, the sine curve on the right is plotted with phase shift
+`phi`; default value = `true`
+
+`marker` If unequal to `""`, a marker is used in the right plot;
+default value = `""`
+
+# Example
+
+Copy and paste code:
+
+```julia
+using Unitful, Unitful.DefaultSymbols, PyPlot, ElectricalEngineering
+phasorsine(1, 0°, ylabel=L"$i$", maglabel=L"$\hat{I}$",
+labelrelrot = false, color="black", linewidth=1.5, xlabel=L"\beta/{^\circ}",
+shift = false, marker = "o")
+subplot(121)
+phasor(pol(1,30°),color=colorBlack2,linestyle=lineStyle2)
+phasorsine_hline(1, 30°, color=colorBlack2, shift=false, marker="o")
+```
+"""
+function phasorsine_hline(
+    mag = 1,
+    phi = 0;
+    color="black",
+    linewidth = 1,
+    linestyle = ":",
+    colorDash="gray",
+    shift = true,
+    marker="")
+
+    # Consider optional phase shift
+    phishift = shift ? upstrip(phi) : 0
+    # Limit of dashed line
+    philim = shift ? 0 : upstrip(phi)
+    # Caution: ustrip(phi) is used instead of phi to allow specification of
+    #          angle phi with unit degree (°)
+    # Determine axis handler of subplots 122 and 121
+    subplot(121)
+    ax1 = gca()
+    # Dotted line from phasor arrow to begin of cosine wave, split in two
+    subplot(122)
+    ax2 = gca()
+    # parts, to avoid overlay effects if multiple dashed lines are drawn
+    con = matplotlib.patches."ConnectionPatch"(
+        xyB=(philim*180/pi, mag*sin(phishift)), xyA=(0, mag*sin(phishift)),
+        coordsA="data", coordsB="data",
+        axesA=ax2, axesB=ax2, color=colorDash,
+        linewidth=lineWidth1, linestyle=":", clip_on=false)
+    ax2.add_artist(con)
+    con = matplotlib.patches."ConnectionPatch"(
+        xyA=(philim*180/pi, mag*sin(phi)), xyB=(mag*cos(phi), mag*sin(phi)),
+        coordsA="data", coordsB="data",
+        axesA=ax2, axesB=ax1, color=colorDash,
+        linewidth=lineWidth1, linestyle=":", clip_on=false)
+    ax2.add_artist(con)
+    if marker != ""
+        subplot(122)
+        plot(philim*180/pi, mag*sin(phi),
+            marker = marker, markersize=markerSize1,
+            color=color, markerfacecolor="w", zorder=100, clip_on=false)
+    end
+end
+
+@doc raw"""
+# Function call
+
+```
+phasorcosine_hline(phasorcosine_hline(mag = 1, phi = 0; color="black",
+linewidth = 1, linestyle = ":", colorDash="gray", shift = true, marker="")
+```
+
+# Description
+
+This function may be applied after using `phasorcosine`. It draws horizontal
+line between a phasor on the left subplot and the cosine wave on the right
+subplot.
+# Variables
+
+`mag` Magnitude of displayed phasor; shall be between 0 and 1;
+default value = 1
+
+`phi` Phase angle of the phasor; default value = 0
+
+`color` Color of the phasor; i.e., shaft and arrow head color; default
+value = "black"
+
+`linewidth` Line width of the horizontal line; default value = 1
+
+`linestyle` Line style of the horizontal line; default value = ":"
+
+`colorDash` Color of the dashed circle (left subplot) and the horizontal dashed
+lines between the left and right subplot; default value = "gray"
+
+`shift` If `true`, the cosine curve on the right is plotted with phase shift
+`phi`; default value = `true`
+
+`marker` If unequal to `""`, a marker is used in the right plot;
+default value = `""`
+
+# Example
+
+Copy and paste code:
+
+```julia
+using Unitful, Unitful.DefaultSymbols, PyPlot, ElectricalEngineering
+phasorcosine(1, 0°, ylabel=L"$i$", maglabel=L"$\hat{I}$",
+labelrelrot = false, color="black", linewidth=1.5, xlabel=L"\beta/{^\circ}",
+shift = false, marker = "o")
+subplot(121)
+phasor(j * pol(1,30°),color=colorBlack2,linestyle=lineStyle2)
+phasorcosine_hline(1, 30°, color=colorBlack2, shift=false, marker="o")
+```
+"""
+function phasorcosine_hline(
+    mag = 1,
+    phi = 0;
+    color="black",
+    linewidth = 1,
+    linestyle = ":",
+    colorDash="gray",
+    shift = true,
+    marker="")
+
+    # Consider optional phase shift
+    phishift = shift ? upstrip(phi) : 0
+    # Limit of dashed line
+    philim = shift ? 0 : upstrip(phi)
+    # Caution: ustrip(phi) is used instead of phi to allow specification of
+    #          angle phi with unit degree (°)
+    # Determine axis handler of subplots 122 and 121
+    subplot(121)
+    ax1 = gca()
+    # Dotted line from phasor arrow to begin of cosine wave, split in two
+    subplot(122)
+    ax2 = gca()
+    # parts, to avoid overlay effects if multiple dashed lines are drawn
+    con = matplotlib.patches."ConnectionPatch"(
+        xyB=(philim*180/pi, mag*cos(phi)), xyA=(0, mag*cos(phi)),
+        coordsA="data", coordsB="data",
+        axesA=ax2, axesB=ax2, color=colorDash,
+        linewidth=lineWidth1, linestyle=":", clip_on=false)
+    ax2.add_artist(con)
+    con = matplotlib.patches."ConnectionPatch"(
+        xyA=(philim*180/pi, mag*cos(phi)), xyB=(-mag*sin(phi), mag*cos(phi)),
+        coordsA="data", coordsB="data",
+        axesA=ax2, axesB=ax1, color=colorDash,
+        linewidth=lineWidth1, linestyle=":", clip_on=false)
+    ax2.add_artist(con)
+    if marker != ""
+        subplot(122)
+        plot(philim*180/pi, mag*cos(phi),
+            marker = marker, markersize=markerSize1,
+            color=color, markerfacecolor="w", zorder=100, clip_on=false)
+    end
+end
+
+@doc raw"""
+# Function call
+
+```
 angulardimension(r = 1, phi1 = 0, phi2 = pi/2; origin = 0.0im,
     label= "", labelphisep = 0.5, labelrsep = 0.1,
     labelrelrot = false, labelrelangle = 0, ha = "center", va = "center",
